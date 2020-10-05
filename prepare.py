73d7206f93de320c97dbe6cc66e5ae65329588f1
Bibek Mainali <bibek.mainali20@gmail.com>
2020-10-04 22:41:09 -0500

    Prepare module updated

--- prepare.py
+++ prepare.py
@@ -86,7 +86,7 @@ def prep_mall_data(df):
     Takes the acquired mall data, does data preparation, and return train, test, 
     and validate data splits.
     '''
-    df['is_femae'] = (df.gender =='Female').astype('int')
+    df['is_female'] = (df.gender =='Female').astype('int')
     train_validate, test = train_test_split(df, test_size =0.15, random_state = 123)
     train, validate = train_test_split(train_validate, test_size = 0.15, random_state = 123)
     return train, test, validate
